ba0fafb35ddd27769d4c7ec132d8198b4bb5dcd7
Euge <eugelito@outlook.com>
2021-09-28 19:19:40 +0100

    Addition of fa icons

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Eugelito Troyo
+          Hey! I'm Euge.
         </h1>
-        <p>Website creation in progress...</p>
-        <div class="buttons">
-
+        <p className="subtitle">Website creation is in progress, watch this space...</p>
+        <div className="buttons">
         <a
           className="App-btn"
           href="https://www.linkedin.com/in/eugelito/"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        ><i className="fab fa-linkedin-in"></i>
           LinkedIn
         </a>
         <a
@@ -24,7 +23,7 @@ function App() {
           href="mailto:eugelito@outlook.com"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        ><i className="fas fa-envelope"></i>
           Email
         </a>
         </div>
