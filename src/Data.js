d12d81ab6bd27ab83d69c5b0c73a024c8885c142
Eugelito Troyo <eugelito@outlook.com>
2021-11-28 15:21:17 +0000

    latest project first

--- src/Data.js
+++ src/Data.js
@@ -1,30 +1,30 @@
 export const data =[
   {
     "id": 0,
+    "img": process.env.PUBLIC_URL + 'images/acbelfast.png',
+    "alt": "AC Belfast",
+    "title": "AC Belfast Football Club",
+    "technologies": "HTML | SCSS | React",
+    "description": "Website designed and developed for AC Belfast, are a local football club based in Belfast. The club has two squads including a first team and over 35s. ",
+    "urlLink": "https://acbelfast.com/" 
+  },
+  {
+    "id": 1,
     "img": process.env.PUBLIC_URL + 'images/soshealthcareltd.png',
     "alt": "sos healthcare ltd website image",
     "title": "SOS Healthcare Ltd",
-    "technologies": "WordPress",
+    "technologies": "WordPress | Elementor",
     "description": "Informational website for a local nursing agency, SOS Healthcare Ltd recruit and supply registered nurses and healthcare assistants.",
     "urlLink": "https://soshealthcareltd.co.uk/"
   },
   {
-    "id": 1,
+    "id": 2,
     "img": process.env.PUBLIC_URL + 'images/UXUIPortfolio.png',
     "alt": "UX/UI online portfolio website image",
     "title": "UX/UI Portfolio",
     "technologies": "WordPress",
     "description": "An online portfolio showcasing my User Experience (UX) and User Interface (UI) work during my university studies.",
     "urlLink": "https://blogs.city.ac.uk/eugelitotroyo/"
-  },
-  {
-    "id": 2,
-    "img": process.env.PUBLIC_URL + 'images/acbelfast.png',
-    "alt": "AC Belfast",
-    "title": "AC Belfast Football Club",
-    "technologies": "HTML | SCSS | React",
-    "description": "AC Belfast are a local football club based in Belfast. With a firsts and over 35s team.",
-    "urlLink": "https://acbelfast.com/" 
   }
 ]
 
