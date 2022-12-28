b573a2033760fd69286135daec7816294bdd5909
Eugelito Troyo <eugelito@outlook.com>
2022-12-28 15:00:44 +0000

    UXUI portfolio update

--- src/Data.js
+++ src/Data.js
@@ -1,31 +1,34 @@
-export const data =[
+export const data = [
   {
-    "id": 1,
-    "img": process.env.PUBLIC_URL + 'images/acbelfast.png',
-    "alt": "AC Belfast",
-    "title": "Athletic Club Belfast",
-    "technologies": "HTML | SCSS | React",
-    "description": "The website allows visitors to learn more about the club, view upcoming fixtures, recent results and contact details.",
-    "urlLink": "https://acbelfast.com/" 
+    id: 1,
+    img: process.env.PUBLIC_URL + "images/acbelfast.png",
+    alt: "AC Belfast",
+    title: "Athletic Club Belfast",
+    technologies: "HTML | SCSS | React",
+    description:
+      "The website allows visitors to learn more about the club, view upcoming fixtures, recent results and contact details.",
+    urlLink: "https://acbelfast.com/",
   },
   {
-    "id": 2,
-    "img": process.env.PUBLIC_URL + 'images/soshealthcareltd.png',
-    "alt": "sos healthcare ltd website image",
-    "title": "SOS Healthcare Ltd",
-    "technologies": "WordPress | Elementor",
-    "description": "Informational website for a local nursing agency that recruit and supply registered nurses and healthcare assistants across Northern Ireland.",
-    "urlLink": "https://soshealthcareltd.co.uk/home/"
+    id: 2,
+    img: process.env.PUBLIC_URL + "images/bfshealthcarewebsite.png",
+    alt: "BFS Healthcare Ltd logo",
+    title: "BFS Healthcare Ltd",
+    technologies: "WordPress | Elementor",
+    description:
+      "Informational website for a local nursing agency that recruit and supply registered nurses and healthcare assistants across Northern Ireland.",
+    urlLink: "https://bfshealthcare.co.uk/",
   },
   {
-    "id": 3,
-    "img": process.env.PUBLIC_URL + 'images/UXUIPortfolio.png',
-    "alt": "UX/UI online portfolio website image",
-    "title": "UX/UI Portfolio",
-    "technologies": "WordPress",
-    "description": "An online portfolio showcasing my User Experience (UX) and User Interface (UI) work during my university studies.",
-    "urlLink": "https://blogs.city.ac.uk/eugelitotroyo/"
-  }
-]
+    id: 3,
+    img: process.env.PUBLIC_URL + "images/UXUIPortfolio.png",
+    alt: "UX/UI online portfolio website image",
+    title: "UX/UI Portfolio",
+    technologies: "WordPress",
+    description:
+      "An online portfolio showcasing my User Experience (UX) and User Interface (UI) work during my university studies.",
+    urlLink: "https://eugelitouxui.wordpress.com/",
+  },
+];
 
-export default data; 
+export default data;
