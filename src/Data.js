bb6cb86f6634f30ca5f328eb2f0a5ad88da18483
Eugelito Troyo <eugelito@outlook.com>
2022-12-28 22:03:50 +0000

    Update technology used

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ export const data = [
     img: process.env.PUBLIC_URL + "images/acbelfast.png",
     alt: "AC Belfast",
     title: "Athletic Club Belfast",
-    technologies: "HTML | SCSS | React",
+    technologies: "React | Firebase | SCSS",
     description:
       "The website allows visitors to learn more about the club, view upcoming fixtures, recent results and contact details.",
     urlLink: "https://acbelfast.com/",
